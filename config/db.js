53558d8489ec4391c2209067bdd6b8e878ff9b7d
Andrew Mundy <andrewmundy@gmail.com>
2017-05-16 08:11:44 -0700

    api implimentation

--- config/db.js
+++ config/db.js
@@ -1,7 +1,3 @@
 module.exports = {
   url:'mongodb://andrewmundy:unreal@ds151279.mlab.com:51279/jackhandeyapi'
 };
-
-
-
-// mongodb://<adminuser>:<unreal>@ds012345-a0.mlab.com:56789,ds012345-a1.mlab.com:56790/admin?replicaSet=rs-ds012345
